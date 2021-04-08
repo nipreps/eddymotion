71ba6ddc39bbe6a4fb755c5bc7542846a0f4ded0
Oscar Esteban <code@oscaresteban.es>
2021-04-08 22:58:56 +0200

    enh: do not limit visibility of submodules

--- eddymotion/__init__.py
+++ eddymotion/__init__.py
@@ -8,10 +8,3 @@ __url__ = "https://github.com/nipreps/EddyMotionCorrection"
 DOWNLOAD_URL = (
     f"https://github.com/nipreps/{__packagename__}/archive/{__version__}.tar.gz"
 )
-
-
-__all__ = [
-    "__version__",
-    "__copyright__",
-    "__packagename__",
-]

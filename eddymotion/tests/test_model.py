ecb48db2e39906045c3ee76ae26bda5fa7b00419
cprovins <celine.provins@unil.ch>
2021-11-09 16:24:40 +0100

    ENH : Add a test for verifying that the two ways to initialize a model are equivalent

--- eddymotion/tests/test_model.py
+++ eddymotion/tests/test_model.py
@@ -24,7 +24,7 @@
 import pytest
 import numpy as np
 from eddymotion import model
-
+from eddymotion.dmri import DWI
 
 def test_trivial_model():
     """Check the implementation of the trivial B0 model."""
@@ -40,3 +40,40 @@ def test_trivial_model():
     assert tmodel.fit() is None
 
     assert np.all(_S0 == tmodel.predict((1, 0, 0)))
+
+
+def test_two_initialisations(pkg_datadir):
+    """Check that the two different initialisations result in the same models"""
+    
+    # Load test data
+    dmri_dataset = DWI.from_filename((pkg_datadir,"/data/dwi.h5"))
+
+    # Split data into test and train set
+    data_train, data_test = dmri_dataset.logo_split(10)
+
+    # Direct initialisation
+    model1 = model.AverageDWModel(
+        dmri_dataset.gradients,
+        S0=dmri_dataset.bzero,
+        th_low=100,
+        th_high=1000,
+        bias = True,
+        stat = 'mean'
+    )
+    model1.fit(data_train[0])
+    predicted1 = model1.predict(data_test[1])
+
+    # Initialisation via ModelFactory
+    model2 = model.ModelFactory.init(
+        gtab=data_train[1],
+        model="avg",
+        S0=dmri_dataset.bzero,
+        th_low=100,
+        th_high=1000,
+        bias = True,
+        stat = 'mean'
+    )
+    model2.fit(data_train[0])
+    predicted2 = model2.predict(data_test[1])
+
+    assert predicted1 == predicted2

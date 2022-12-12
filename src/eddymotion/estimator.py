b1a09ed0bcdde0f260173b42069cda3b0fd65acc
dpys <dpisner@clairity.com>
2022-12-11 23:48:45 -0600

    [ENH]: Recompose alignment functionality of estimator module into dataclass; debug issue with mask as a kwarg, and replace slicing with np.ma.masked_array; add handling for RuntimeErrors in fit/predict for BaseModel

--- src/eddymotion/estimator.py
+++ src/eddymotion/estimator.py
@@ -23,6 +23,8 @@
 """A model-based algorithm for the realignment of dMRI data."""
 from pathlib import Path
 from tempfile import TemporaryDirectory, mkstemp
+from dataclasses import dataclass
+from typing import Any
 
 import nibabel as nb
 import nitransforms as nt
@@ -42,7 +44,7 @@ class EddyMotionEstimator:
         dwdata,
         *,
         align_kwargs=None,
-        models=("b0", ),
+        models=("b0",),
         omp_nthreads=None,
         n_jobs=None,
         seed=None,
@@ -86,9 +88,9 @@ class EddyMotionEstimator:
         bmask_img = None
         if dwdata.brainmask is not None:
             _, bmask_img = mkstemp(suffix="_bmask.nii.gz")
-            nb.Nifti1Image(
-                dwdata.brainmask.astype("uint8"), dwdata.affine, None
-            ).to_filename(bmask_img)
+            nb.Nifti1Image(dwdata.brainmask.astype("uint8"), dwdata.affine, None).to_filename(
+                bmask_img
+            )
             kwargs["mask"] = dwdata.brainmask
 
         kwargs["S0"] = _advanced_clip(dwdata.bzero)
@@ -96,20 +98,23 @@ class EddyMotionEstimator:
         if "num_threads" not in align_kwargs and omp_nthreads is not None:
             align_kwargs["num_threads"] = omp_nthreads
 
+        astore = AlignStore(dwdata, reg_target_type, bmask_img, align_kwargs)
+
         n_iter = len(models)
         for i_iter, model in enumerate(models):
             reg_target_type = (
-                "dwi"
-                if model.lower() not in ("b0", "s0", "avg", "average", "mean")
-                else "b0"
+                "dwi" if model.lower() not in ("b0", "s0", "avg", "average", "mean") else "b0"
             )
             index_order = np.arange(len(dwdata))
             np.random.shuffle(index_order)
 
-            single_model = (
-                model.lower() in ("b0", "s0", "avg", "average", "mean")
-                or model.lower().startswith("full")
-            )
+            single_model = model.lower() in (
+                "b0",
+                "s0",
+                "avg",
+                "average",
+                "mean",
+            ) or model.lower().startswith("full")
 
             dwmodel = None
             if single_model:
@@ -126,14 +131,15 @@ class EddyMotionEstimator:
 
             with TemporaryDirectory() as tmpdir:
                 print(f"Processing in <{tmpdir}>")
+
                 with tqdm(total=len(index_order), unit="dwi") as pbar:
                     # run a original-to-synthetic affine registration
-                    for i in index_order:
+                    for b_ix in index_order:
                         pbar.set_description_str(
-                            f"Pass {i_iter + 1}/{n_iter} | Fit and predict b-index <{i}>"
+                            f"Pass {i_iter + 1}/{n_iter} | Fit and predict b-index <{b_ix}>"
                         )
-                        data_train, data_test = dwdata.logo_split(i, with_b0=True)
-                        grad_str = f"{i}, {data_test[1][:3]}, b={int(data_test[1][3])}"
+                        data_train, data_test = dwdata.logo_split(b_ix, with_b0=True)
+                        grad_str = f"{b_ix}, {data_test[1][:3]}, b={int(data_test[1][3])}"
                         pbar.set_description_str(f"[{grad_str}], {n_jobs} jobs")
 
                         if not single_model:  # A true LOGO estimator
@@ -156,8 +162,8 @@ class EddyMotionEstimator:
 
                         # prepare data for running ANTs
                         tmpdir = Path(tmpdir)
-                        moving = tmpdir / f"moving{i:05d}.nii.gz"
-                        fixed = tmpdir / f"fixed{i:05d}.nii.gz"
+                        moving = tmpdir / f"moving{b_ix:05d}.nii.gz"
+                        fixed = tmpdir / f"fixed{b_ix:05d}.nii.gz"
                         _to_nifti(data_test[0], dwdata.affine, moving)
                         _to_nifti(
                             predicted,
@@ -167,51 +173,68 @@ class EddyMotionEstimator:
                         )
 
                         pbar.set_description_str(
-                            f"Pass {i_iter + 1}/{n_iter} | Realign b-index <{i}>"
+                            f"Pass {i_iter + 1}/{n_iter} | Realign b-index <{b_ix}>"
                         )
-                        registration = Registration(
-                            terminal_output="file",
-                            from_file=pkg_fn(
-                                "eddymotion",
-                                f"config/dwi-to-{reg_target_type}_level{i_iter}.json",
-                            ),
-                            fixed_image=str(fixed.absolute()),
-                            moving_image=str(moving.absolute()),
-                            **align_kwargs,
-                        )
-                        if bmask_img:
-                            registration.inputs.fixed_image_masks = ["NULL", bmask_img]
-
-                        if dwdata.em_affines and dwdata.em_affines[i] is not None:
-                            mat_file = tmpdir / f"init_{i_iter}_{i:05d}.mat"
-                            dwdata.em_affines[i].to_filename(mat_file, fmt="itk")
-                            registration.inputs.initial_moving_transform = str(mat_file)
-
-                        # execute ants command line
-                        result = registration.run(cwd=str(tmpdir)).outputs
-
-                        # read output transform
-                        xform = nt.linear.Affine(
-                            nt.io.itk.ITKLinearTransform.from_filename(
-                                result.forward_transforms[0]
-                            ).to_ras(reference=fixed, moving=moving),
-                        )
-                        # debugging: generate aligned file for testing
-                        xform.apply(moving, reference=fixed).to_filename(
-                            tmpdir / f"aligned{i:05d}_{int(data_test[1][3]):04d}.nii.gz"
-                        )
+                        astore.fit(tmpdir, data_test, fixed, moving)
+                        xform = astore.transform(b_ix, i_iter)
 
                         # update
-                        dwdata.set_transform(i, xform.matrix)
+                        dwdata.set_transform(b_ix, xform.matrix)
                         pbar.update()
 
         return dwdata.em_affines
 
 
-def _advanced_clip(
-    data, p_min=35, p_max=99.98, nonnegative=True, dtype="int16", invert=False
-):
-    """
+@dataclass
+class AlignStore:
+    dwdata: Any
+    reg_target_type: Any
+    bmask_img: Any
+    align_kwargs: Any
+
+    def fit(self, tmpdir, data_test, fixed, moving):
+        self.data_test = data_test
+        self.fixed = fixed
+        self.moving = moving
+        self.tmpdir = tmpdir
+
+    def transform(self, b_ix, i_iter):
+        registration = Registration(
+            terminal_output="file",
+            from_file=pkg_fn(
+                "eddymotion",
+                f"config/dwi-to-{self.reg_target_type}_level{i_iter}.json",
+            ),
+            fixed_image=str(self.fixed.absolute()),
+            moving_image=str(self.moving.absolute()),
+            **self.align_kwargs,
+        )
+        if self.bmask_img:
+            registration.inputs.fixed_image_masks = ["NULL", self.bmask_img]
+
+        if self.dwdata.em_affines and self.dwdata.em_affines[b_ix] is not None:
+            mat_file = self.tmpdir / f"init_{i_iter}_{b_ix:05d}.mat"
+            self.dwdata.em_affines[b_ix].to_filename(mat_file, fmt="itk")
+            registration.inputs.initial_moving_transform = str(mat_file)
+
+        # execute ants command line
+        result = registration.run(cwd=str(self.tmpdir)).outputs
+
+        # read output transform
+        xform = nt.linear.Affine(
+            nt.io.itk.ITKLinearTransform.from_filename(result.forward_transforms[0]).to_ras(
+                reference=self.fixed, moving=self.moving
+            ),
+        )
+        # debugging: generate aligned file for testing
+        xform.apply(self.moving, reference=self.fixed).to_filename(
+            self.tmpdir / f"aligned{b_ix:05d}_{int(self.data_test[1][3]):04d}.nii.gz"
+        )
+        return xform
+
+
+def _advanced_clip(data, p_min=35, p_max=99.98, nonnegative=True, dtype="int16", invert=False):
+    r"""
     Remove outliers at both ends of the intensity distribution and fit into a given dtype.
 
     This interface tries to emulate ANTs workflows' massaging that truncate images into
@@ -231,6 +254,9 @@ def _advanced_clip(
 
     # Calculate stats on denoised version, to preempt outliers from biasing
     denoised = ndimage.median_filter(data, footprint=ball(3))
+
+    if len(denoised[denoised > 0]) == 0:
+        return data
 
     a_min = np.percentile(denoised[denoised > 0] if nonnegative else denoised, p_min)
     a_max = np.percentile(denoised[denoised > 0] if nonnegative else denoised, p_max)

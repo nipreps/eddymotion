1e305d9b4aeb298e3e9ce27e36013cabebe2d03b
esavary <elodie.savary@outlook.com>
2024-04-04 16:29:42 +0200

    fix:import statement

--- src/eddymotion/estimator.py
+++ src/eddymotion/estimator.py
@@ -36,7 +36,7 @@ from tqdm import tqdm
 
 from eddymotion.data.splitting import lovo_split
 from eddymotion.model import ModelFactory
-from utils import sort_dwdata_indices, SortingStrategy
+from eddymotion.utils import sort_dwdata_indices, SortingStrategy
 
 
 class EddyMotionEstimator:

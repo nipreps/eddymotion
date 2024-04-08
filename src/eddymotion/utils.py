6f0d5fe6cd99e2dfd58eeb7eb729dd4422c40ca7
esavary <elodie.savary@outlook.com>
2024-04-08 15:10:18 +0200

    fix: remove unused import

--- src/eddymotion/utils.py
+++ src/eddymotion/utils.py
@@ -1,6 +1,6 @@
 import random
 from itertools import chain, zip_longest
-from typing import Generator, List, Union
+from typing import Generator
 
 # emacs: -*- mode: python; py-indent-offset: 4; indent-tabs-mode: nil -*-
 # vi: set ft=python sts=4 ts=4 sw=4 et:
